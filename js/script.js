db6fd9a89ebebaee28bdf52742071855a5112853
trinhnguyen212 <tngu212@gmail.com>
2023-10-31 17:48:48 +1300

    typing animation feature

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-/* ====================================== typing animation ========================*/
-var typed = new Typed(".typing",{
-    strings:["",""]
-})
+/* /* ====================================== typing animation ========================*/
+var typed = new Typed('.typing', {
+    strings: ["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
